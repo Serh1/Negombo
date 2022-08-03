f832c58b4859516ea3e1dde144522ab0144e6c53
Manea-Alex <alexmanea2201@gmail.com>
2022-08-03 16:47:29 +0300

    Frontend nav bar style

--- negombo/resources/views/layouts/usermaster.blade.php
+++ negombo/resources/views/layouts/usermaster.blade.php
@@ -82,12 +82,13 @@
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown"
                    aria-haspopup="true" aria-expanded="false">
-                    {{ Config::get('languages')[App::getLocale()] }}
+                    {{-- {{ Config::get('languages')[App::getLocale()] }} --}}
+                    <span>{{ Config::get('languages')[App::getLocale()] }}</span>
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                     @foreach (Config::get('languages') as $lang => $language)
                         @if ($lang != App::getLocale())
-                            <a class="dropdown-item" href="{{ route('lang.switch', $lang) }}"> {{$language}}</a>
+                            <a class="dropdown-item" href="{{ route('lang.switch', $lang) }}">  <span>{{ $language}}</span></a>
                         @endif
                     @endforeach
                 </div>
